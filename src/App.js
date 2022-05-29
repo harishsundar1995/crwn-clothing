d4316a8e3e3a2412cdb7befd62ade694e6520e27
Harish Sundar <harishsundarg@gmail.com>
2022-05-29 09:40:10 +0530

    Initial App Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import categories from './categories.json'
+import Directory from './components/directory/directory.component'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <Directory categories={categories} />
+    </div >
+  )
 }
 
-export default App;
+export default App
