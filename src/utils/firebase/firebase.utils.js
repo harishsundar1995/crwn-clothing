eba1bd487860f5b9b929c22a58ed85838092ccff
harishsundar1995 <harishsundar1995@gmail.com>
2022-05-30 18:42:41 +0530

    firebase updated

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,7 @@
+import { async } from '@firebase/util';
 import { initializeApp } from 'firebase/app'
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -24,3 +26,14 @@ provider.setCustomParameters({
 export const auth = getAuth()
 
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore()
+
+export const createUserDocumentFromAuth = async (userAuth) =>{
+    const userDocRef = doc(db,'users', userAuth.uid)
+
+    console.log(userDocRef)
+
+    const userSnapshot = await getDoc(userDocRef)
+    console.log(userSnapshot.exists())
+} 
