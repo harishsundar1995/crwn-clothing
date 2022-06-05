ef8c3b800108ace61a44425d655f4ce1c802c725
Harish Sundar <harishsundarg@gmail.com>
2022-06-05 10:31:33 +0530

    Checkout Page Completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Authentication from './components/routes/authentication/authentication.component'
+import Checkout from './components/routes/checkout/checkout.component'
 import Home from './components/routes/home/home.component'
 import Navigation from './components/routes/navigation/navigation.component'
 import Shop from './components/routes/shop/shop.component'
@@ -11,6 +12,7 @@ const App = () => {
         <Route index={true} element={<Home />} />
         <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes >
   )
