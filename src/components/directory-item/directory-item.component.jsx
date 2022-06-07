5d51c1c8623da410f2e9ae3acaed85af05d49122
Harish Sundar <harishsundarg@gmail.com>
2022-06-07 07:34:49 +0530

    Basic Features Completed

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import './category-item.styles.scss'
+import './directory-item.styles.scss'
 
-const CategoryItem = ({ category }) => {
+const DirectoryItem = ({ category }) => {
     const { title, id, imageUrl } = category
     return (
-        <div className='category-container' key={id}>
+        <div className='directory-item-container' key={id}>
             <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
-            <div div className='category-body-container' >
+            <div div className='body ' >
                 <h2>{title}</h2>
                 <p>Shop Now</p>
             </div>
@@ -14,4 +14,4 @@ const CategoryItem = ({ category }) => {
     )
 }
 
-export default CategoryItem
+export default DirectoryItem
